9d1bfa77fc10696576c8d2fe8b32b7b223be437f
Moi.warez <Moi.warez@gmail.com>
2015-01-02 01:30:25 +0000

    Mise au point de la construction du breadcrumb pour le catalogue basé sur l'url courante

--- lib/Cible/View/Helper/BreadcrumbCatalog.php
+++ lib/Cible/View/Helper/BreadcrumbCatalog.php
@@ -32,73 +32,91 @@ class Cible_View_Helper_BreadcrumbCatalog extends Zend_View_Helper_Abstract
     public function breadcrumbCatalog($level = 1, $showHome = true, $langId = null, $options = array())
     {
 
+        $_breadcrumb = array();
+        $this->first = true;
+
         if( $langId == null )
             $langId = Zend_Registry::get('languageID');
-
-        $_baseUrl = Zend_Registry::get('baseUrl');
-
-        $_breadcrumb = array();
-        $_first = true;
+        $this->langId = $langId;
 
         $pathInfo  = $this->view->request->getPathInfo();
         $oProducts = new CatalogCollection();
-
         $oProducts->setActions($pathInfo);
         $oProducts->getDataByName();
 
         $subCatId = null;
-        $catId    = $oProducts->getCatId();
-        $prodId   = $oProducts->getProdId();
-        $details = Cible_FunctionsPages::getPageDetails($this->view->currentPageID, $langId);
-        $pageLink = "<a href='{$_baseUrl}/{$details['PI_PageIndex']}'>{$details['PI_PageTitle']}</a>";
-        if ($catId == null && $subCatId == null && $prodId == null)
+        $this->catId = $oProducts->getCatId();
+        $this->prodId = $oProducts->getProdId();
+        $details = Cible_FunctionsPages::getPageDetails($this->view->currentPageID, $this->langId);
+        $pageLink = $this->view->link($this->view->baseUrl() . '/' .$details['PI_PageIndex'],$details['PI_PageTitle']);
+        if ($this->catId == null && $subCatId == null && $this->prodId == null)
         {
-//            $_breadcrumb = $this->view->breadcrumb(true);
             $_breadcrumb[0] = $this->view->pageTitle()->toString(null, null, true);
             return  $_breadcrumb;
         }
         else
         {
-            $pathElemts = $oProducts->getActions();
-            if($prodId)
+            $this->pathElemts = $oProducts->getActions();
+            $tmp = array_reverse($this->pathElemts, true);
+            foreach( $tmp as $key => $value)
             {
-                $_class = '';
-                $product = new ProductsObject();
-                $details = $product->populate($prodId, $langId);
-                if( $_first ){$_class = 'current_page';}
-                $link = $_first ? $details['PI_Name']: "<a href='{$_baseUrl}/{$pathElemts[0]}/{$pathElemts[1]}/{$pathElemts[2]}' class='{$_class}'>{$details['PI_Name']}</a>";
-                array_push($_breadcrumb, $link);
-                if( $_first ){$_first = false;}
+                if ($value != $details['PI_PageIndex']){
+                    $link = $this->_getLinks($value);
+                    unset($this->pathElemts[$key]);
+                    array_push($_breadcrumb, $link);
+                }
             }
-
-            if($subCatId)
-            {
-                $_class = '';
-                $oCatalog = new CatalogCollection();
-                $buildOnObj = $oCatalog->getBuildSubMenuOn();
-                $object = new $buildOnObj();
-                $details = $object->populate($subCatId, $langId);
-                if( $_first ){$_class = 'current_page';}
-                $link = $_first ? $details[$object->getTitleField()] : "<a href='{$_baseUrl}/{$pathElemts[0]}/{$pathElemts[1]}' class='{$_class}'>{$details[$object->getTitleField()]}</a>";
-                array_push($_breadcrumb, $link);
-                array_push($_breadcrumb, $pageLink);
-                if( $_first ){$_first = false;}
-            }
-
-            if($catId)
-            {
-                $_class = '';
-                $object = new CatalogCategoriesObject();
-                $details = $object->populate($catId, $langId);
-                if( $_first ){$_class = 'current_page';}
-                $link = $_first ? $details['CCI_Name'] : "<a href='{$_baseUrl}/{$pathElemts[0]}' class='{$_class}'>{$details['CCI_Name']}</a>";
-                array_push($_breadcrumb, $link);
-                if( $_first ){$_first = false;}
-
-            }
-
+            array_push($_breadcrumb, $pageLink);
             return $_breadcrumb;
-
         }
+    }
+
+    private function _getLinks($value)
+    {
+        if ($this->prodId > 0){
+            $link = $this->_getProductLink($value);
+            $this->prodId = 0;
+        }else{
+            $link = $this->_getCategoriesLink($value);
+        }
+
+        return $link;
+    }
+
+    private function _getProductLink($value)
+    {
+        $_class = '';
+        $obj = new ProductsObject();
+        $details = $obj->populate($this->prodId, $this->langId);
+        if( $this->first){
+            $_class = 'current_page';
+            $link = $details[$obj->getTitleField()];
+            $this->first = false;
+
+        }else{
+            $href = $this->view->baseUrl() . implode('/', $this->pathElemts);
+            $link = $this->view->link($href, $details[$obj->getTitleField()]);
+        }
+
+        return $link;
+    }
+
+    private function _getCategoriesLink($value)
+    {
+        $_class = '';
+        $obj = new CatalogCategoriesObject();
+        if( $this->first){
+            $details = $obj->populate($this->catId, $this->langId);
+            $_class = 'current_page';
+            $link = $details[$obj->getTitleField()];
+            $this->first = false;
+        }else{
+            $id = $obj->getIdByName($value);
+            $details = $obj->populate($id, $this->langId);
+            $href = $this->view->baseUrl() . implode('/', $this->pathElemts);
+            $link = $this->view->link('/' . $href, $details[$obj->getTitleField()]);
+        }
+
+        return $link;
     }
 }
